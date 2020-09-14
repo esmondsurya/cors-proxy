5c572100ac7feda4a90805c5e0c28933b71b1ca3
esmondsurya <69661292+esmondsurya@users.noreply.github.com>
2020-09-14 18:25:46 +0700

    Update server.js
    change preflight

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
-    if (req.method === 'OPTIONS') {
+    //if (req.method === 'OPTIONS') {
         // CORS Preflight
-        res.send();
-    } else {
+        //res.send();
+   // } else {
         var targetURL = req.header('Target-URL');
         if (!targetURL) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
